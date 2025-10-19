ff2ad03695f08cf44664470850bb33689e9aa0ab
Atik Sharma <atiksharma1503@gmail.com>
2025-10-19 10:59:04 +0530

    Code Refactoring

--- src/main/java/com/taskmanager/auth/mapper/UserBOMapper.java
+++ src/main/java/com/taskmanager/auth/mapper/UserBOMapper.java
@@ -1,26 +1,11 @@
 package com.taskmanager.auth.mapper;
 
-import java.util.Date;
-
-import io.jsonwebtoken.Claims;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
-import com.taskmanager.auth.model.RefreshToken;
-import com.taskmanager.common.model.JwtToken;
 import com.taskmanager.common.model.UserBase;
-import com.taskmanager.common.model.UserExp;
 import com.taskmanager.common.model.request.LoginRequest;
-import com.taskmanager.common.model.request.RegistrationRequest;
+import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserBOMapper {
-
-    public RegistrationRequest mapToRegistrationRequestFromUserBase(UserBase userBase);
-
-    public UserBase mapFromRegistrationRequestToUserBase(RegistrationRequest registrationRequest);
-
-    public UserExp mapToExpFromUserBase(UserBase user);
 
     public UserBase mapFromLoginRequest(LoginRequest loginRequest);
 
